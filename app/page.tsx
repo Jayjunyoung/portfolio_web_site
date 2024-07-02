b28e4b079028a97edee2931ab4da7b2f9a3bf129
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2024-07-02 15:21:52 +0900

    feat: 마우스 위치 따라가는 애니메이션 및 메인화면 애니메이션 구현

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,42 @@
+"use client";
+
 import Header from "@/components/Header";
-import Image from "next/image";
+import { useEffect } from "react";
 
 export default function MainPage() {
+  useEffect(() => {
+    const circle = document.getElementById("circle");
+
+    if (circle) {
+      const onMouseMove = (e: { clientX: number; clientY: number }) => {
+        circle.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
+      };
+      document.addEventListener("mousemove", onMouseMove);
+
+      return () => {
+        document.removeEventListener("mousemove", onMouseMove);
+      };
+    }
+  }, []);
+
+  useEffect(() => {
+    const fadeInElements = document.querySelectorAll(".fade-in");
+    fadeInElements.forEach((element) => {
+      element.classList.add("fade-in-animation");
+    });
+  }, []);
+
   return (
-    <>
-      <div className="w-full h-100vh">
-        <Header />
-        <div>안녕</div>
+    <div className="w-full h-screen bg-black relative flex flex-col text-white fade-in">
+      <div className="absolute top-0 left-0 w-full h-full stars"></div>
+      <Header />
+      <div className="flex justify-center items-center flex-grow relative z-10">
+        <div className="flex justify-center items-center w-full h-[250px]">
+          <span className="text-2xl">정준영의 포트폴리오</span>
+        </div>
       </div>
-    </>
+      <div id="circle" className="circle"></div>
+      <div className="absolute top-0 left-0 w-full h-full stars-flipped"></div>
+    </div>
   );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -2,11 +2,23 @@
 
 export default function Header() {
   return (
-    <div className="flex justify-evenly items-center w-full h-auto">
-      <div className="flex items-center h-[150px]">FrontEnd-Enginner</div>
-      <div className="flex items-center h-[150px]">about</div>
-      <div className="flex items-center h-[150px]">projects</div>
-      <div className="flex items-center h-[150px]">contact</div>
+    <div className="flex justify-evenly items-center w-full h-[150px] text-3xl z-10">
+      <div className="flex items-center w-auto relative group">
+        <span>FrontEnd-Engineer</span>
+        <div className="absolute bottom-0 left-0 w-0 h-0.5 bg-white transition-all duration-300 group-hover:w-full"></div>
+      </div>
+      <div className="flex items-center relative group">
+        <span>about</span>
+        <div className="absolute bottom-0 left-0 w-0 h-0.5 bg-white transition-all duration-300 group-hover:w-full"></div>
+      </div>
+      <div className="flex items-center relative group">
+        <span>projects</span>
+        <div className="absolute bottom-0 left-0 w-0 h-0.5 bg-white transition-all duration-300 group-hover:w-full"></div>
+      </div>
+      <div className="flex items-center relative group">
+        <span>contact</span>
+        <div className="absolute bottom-0 left-0 w-0 h-0.5 bg-white transition-all duration-300 group-hover:w-full"></div>
+      </div>
     </div>
   );
 }
