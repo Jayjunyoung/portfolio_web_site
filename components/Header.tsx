679a6fd199a228d0522b95911ee4e7fa72d8a80f
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2024-07-02 11:55:33 +0900

    chore: 프로젝트 초기 세팅 시작

---
+++ components/Header.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+export default function Header() {
+  return (
+    <div className="flex justify-evenly items-center w-full h-auto">
+      <div className="flex items-center h-[150px]">FrontEnd-Enginner</div>
+      <div className="flex items-center h-[150px]">about</div>
+      <div className="flex items-center h-[150px]">projects</div>
+      <div className="flex items-center h-[150px]">contact</div>
+    </div>
+  );
+}
