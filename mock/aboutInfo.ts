8f6a9fc1a97aa3f8d293c70755b644075d039b54
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2024-07-07 13:56:24 +0900

    refactor: about 페이지 mock 데이터로 분리

---
+++ mock/aboutInfo.ts
@@ -0,0 +1,25 @@
+export const introductionTexts = [
+  "안녕하세요, 저는 배움이 취미인 프론트엔드 개발자 정준영 입니다.",
+  "하루에 개발한 내용과 알고리즘 문제 해결 과정을 블로그로 작성 중 입니다.",
+  "프론트엔드 개발 직무를 목표로 공부하고 있습니다.",
+  "하루에 해야 할 태스크를 일정관리 툴로 작성하여 계획적으로 완료하는 성격을 가지고 있습니다.",
+];
+
+export const stacksInfo = [
+  {
+    category: "Framework",
+    stacks: ["React.js", "Next.js"],
+  },
+  {
+    category: "State-management",
+    stacks: ["zustand", "redux", "recoil"],
+  },
+  {
+    category: "Languages",
+    stacks: ["javascript", "typescript"],
+  },
+  {
+    category: "CSS",
+    stacks: ["tailwind.css", "Styled-component"],
+  },
+];
