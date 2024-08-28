a83339f45f9d2bb0dee3aba0c7f620f767e6b658
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2024-08-28 15:52:28 +0900

    feat: 그룹화를 통한 프로젝트 화면 슬라이드 구현 및 프로젝트 추가

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -10,6 +10,9 @@ export default function ProjectsPage() {
   const [flippedCards, setFlippedCards] = useState<number[]>([]);
   const [currentSlide, setCurrentSlide] = useState<number[]>([]);
   const [showMessage, setShowMessage] = useState<boolean>(false);
+  const [currentGroup, setCurrentGroup] = useState<number>(0);
+  const projectsPerPage = 3; // 슬라이드당 보여줄 프로젝트 개수는 3개로 설정 완료!
+  const totalGroups = Math.ceil(projectInfo.length / projectsPerPage);
 
   useEffect(() => {
     const pageElement = document.getElementById("project-page");
@@ -68,6 +71,23 @@ export default function ProjectsPage() {
     });
   };
 
+  const handlePrevGroup = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e.stopPropagation();
+    setCurrentGroup((prevGroup) =>
+      prevGroup > 0 ? prevGroup - 1 : totalGroups - 1
+    );
+  };
+
+  const handleNextGroup = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    setCurrentGroup((prevGroup) =>
+      prevGroup < totalGroups - 1 ? prevGroup + 1 : 0
+    );
+  };
+
   return (
     <div
       id="project-page"
@@ -77,82 +97,101 @@ export default function ProjectsPage() {
       <h1 className="text-4xl mb-10 text-center">Projects</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
         {isLoaded &&
-          projectInfo.map((project, index) => (
-            <div
-              key={project.projectName}
-              className={`bg-gray-800 p-5 rounded-lg shadow-lg project-card h-auto cursor-pointer transform ${
-                flippedCards.includes(index) ? "flipped" : ""
-              }`}
-              onClick={() => handleCardClick(index)}
-            >
-              <div className="card-front relative w-full h-full">
-                <h2 className="text-2xl text-center mb-4">
-                  {project.projectName}
-                </h2>
-                <div className="text-sm text-center mb-4 w-full">
-                  {project.summary}
+          projectInfo
+            .slice(
+              currentGroup * projectsPerPage,
+              (currentGroup + 1) * projectsPerPage
+            )
+            .map((project, localIndex) => {
+              const projectIndex = currentGroup * projectsPerPage + localIndex;
+
+              return (
+                <div
+                  key={project.projectName}
+                  className={`bg-gray-800 p-5 rounded-lg shadow-lg project-card h-[564px] border-box cursor-pointer transform ${
+                    flippedCards.includes(projectIndex) ? "flipped" : ""
+                  }`}
+                  onClick={() => handleCardClick(projectIndex)}
+                >
+                  <div className="card-front relative w-full h-full">
+                    <h2 className="text-2xl text-center mb-4">
+                      {project.projectName}
+                    </h2>
+                    <div className="text-sm text-center mb-4 w-full">
+                      {project.summary}
+                    </div>
+                    <div className="text-sm text-center mb-6 w-full text-sky-200 font-bold">
+                      📆 작업기간 : {project.period}
+                    </div>
+                    <div className="text-sm mb-6 w-full font-black">
+                      🙋🏻 인원 : {project.members}명
+                    </div>
+                    <h3 className="text-xl mb-2">Tasks</h3>
+                    <ul className="list-disc list-inside mb-4 min-h-[72px]">
+                      {project.tasks.map((task, idx) => (
+                        <li key={idx}>{task}</li>
+                      ))}
+                    </ul>
+                    <h3 className="text-xl mb-2">Stacks</h3>
+                    <ul className="list-disc list-inside h-auto">
+                      {project.stacks.map((stack, idx) => (
+                        <li key={idx}>{stack}</li>
+                      ))}
+                    </ul>
+                    <div className="absolute bottom-2 right-5 h-auto px-2">
+                      <FaGithub
+                        className={`text-3xl cursor-pointer hover:scale-125 transform transition 0.2s`}
+                        onClick={() => handleIconClick(project.githubUrl)}
+                      />
+                    </div>
+                  </div>
+                  <div className="card-back">
+                    <div className="relative w-full h-full flex items-center justify-center">
+                      <button
+                        onClick={(e) => {
+                          e.stopPropagation();
+                          handlePrevSlide(projectIndex);
+                        }}
+                        className="absolute left-0 text-white z-20"
+                      >
+                        <IoIosArrowBack size={24} />
+                      </button>
+                      <img
+                        src={project.images[currentSlide[projectIndex]]}
+                        alt="Project Image"
+                        className="w-5/6 h-[450px] object-contain rounded-lg"
+                      />
+                      <button
+                        onClick={(e) => {
+                          e.stopPropagation();
+                          handleNextSlide(projectIndex);
+                        }}
+                        className="absolute right-0 text-white z-20"
+                      >
+                        <IoIosArrowForward size={24} />
+                      </button>
+                    </div>
+                  </div>
                 </div>
-                <div className="text-sm text-center mb-6 w-full text-sky-200 font-bold">
-                  📆 작업기간 : {project.period}
-                </div>
-                <div className="text-sm mb-6 w-full font-black">
-                  🙋🏻 인원 : {project.members}명
-                </div>
-                <h3 className="text-xl mb-2">Tasks</h3>
-                <ul className="list-disc list-inside mb-4 min-h-[72px]">
-                  {project.tasks.map((task, idx) => (
-                    <li key={idx}>{task}</li>
-                  ))}
-                </ul>
-                <h3 className="text-xl mb-2">Stacks</h3>
-                <ul className="list-disc list-inside h-auto">
-                  {project.stacks.map((stack, idx) => (
-                    <li key={idx}>{stack}</li>
-                  ))}
-                </ul>
-                <div className="absolute bottom-2 right-5 h-auto px-2">
-                  <FaGithub
-                    className={`text-3xl cursor-pointer hover:scale-125 transform transition 0.2s`}
-                    onClick={() => handleIconClick(project.githubUrl)}
-                  />
-                </div>
-              </div>
-              <div className="card-back">
-                <div className="relative w-full h-full flex items-center justify-center">
-                  <button
-                    onClick={(e) => {
-                      e.stopPropagation();
-                      handlePrevSlide(index);
-                    }}
-                    className="absolute left-0 text-white z-20"
-                  >
-                    <IoIosArrowBack size={24} />
-                  </button>
-                  <img
-                    src={project.images[currentSlide[index]]}
-                    alt="Project Image"
-                    className="w-5/6 h-auto object-cover rounded-lg"
-                  />
-                  <button
-                    onClick={(e) => {
-                      e.stopPropagation();
-                      handleNextSlide(index);
-                    }}
-                    className="absolute right-0 text-white z-20"
-                  >
-                    <IoIosArrowForward size={24} />
-                  </button>
-                </div>
-              </div>
-            </div>
-          ))}
+              );
+            })}
       </div>
       <div
-        className={`w-full h-[280px] text-center flex justify-center items-center text-3xl transition-opacity duration-1000 ${
+        className={`w-full h-[280px] text-center flex justify-around items-center text-3xl transition-opacity duration-1000 ${
           showMessage ? "opacity-100" : "opacity-0"
         }`}
       >
+        <div className="z-20">
+          <button onClick={handlePrevGroup}>
+            <IoIosArrowBack size={36} />
+          </button>
+        </div>
         Click the Card
+        <div className="z-20">
+          <button onClick={handleNextGroup}>
+            <IoIosArrowForward size={36} />
+          </button>
+        </div>
       </div>
       <style jsx>{`
         .page-slide-in {
