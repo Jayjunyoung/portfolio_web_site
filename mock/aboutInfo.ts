af61af5cde1f3e8f21d76e350593c57331036c70
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2024-08-31 14:52:51 +0900

    chore: 소개 데이터 일부 수정

--- mock/aboutInfo.ts
+++ mock/aboutInfo.ts
@@ -8,7 +8,7 @@ export interface IntroductionInfo {
 
 export const introductionTexts: IntroductionInfo["introductionTexts"] = [
   "안녕하세요, 저는 배움이 취미인 프론트엔드 개발자 정준영 입니다.",
-  "프론트엔드 개발 내용과 알고리즘 문제 해결 과정을 블로그로 작성 중이고 현재 90개 이상의 블로그를 작성했습니다.",
+  "프론트엔드 개발 내용과 알고리즘 문제 해결 과정을 블로그로 작성 중이고 현재 90+개 블로그를 작성했습니다.",
   "하루에 해야 할 태스크를 일정관리 툴로 작성하여 계획적으로 완료하는 성격을 가지고 있습니다.",
 ];
 
@@ -31,11 +31,11 @@ export const stacksInfo: IntroductionInfo["stacksInfo"] = [
   },
   {
     category: "BackEnd & DB",
-    stacks: ["Convex", "Workbench", "MySQL"],
+    stacks: ["Convex", "Workbench", "MySQL", "Prisma"],
   },
   {
     category: "Collaboration",
-    stacks: ["Notion", "Figma", "GitHub", "Slack", "ZEP", "Discord"],
+    stacks: ["Notion", "Figma", "GitHub", "Slack", "ZEP", "Discord", "Trello"],
   },
   {
     category: "Deployment",
@@ -54,7 +54,7 @@ export const experienceInfo: IntroductionInfo["experienceInfo"] = [
     period: "2022.10.07 ~ 2023.08.31",
     details: [
       "Google Developer Student Clubs 동아리",
-      "FrontEnd 담당으로 2개의 프로젝트 진행",
+      "FrontEnd 담당으로 1개의 프로젝트 진행",
       "React.js, TypeScript 등 스터디에 참여하여 프론트엔드 개발 학습",
     ],
   },
