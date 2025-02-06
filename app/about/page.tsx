b2426bb01bcb62d47a75713bdc65300142a7832a
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2025-02-06 15:07:51 +0900

    fix: threshold 설정을 적용해 Tech & Stack 섹션이 보이는 이슈 해결

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -79,13 +79,16 @@ export default function AboutPage() {
 
   useEffect(() => {
     const sections = document.querySelectorAll(".section");
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          entry.target.classList.add("show");
-        }
-      });
-    });
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+          }
+        });
+      },
+      { threshold: 0.3 }
+    );
     sections.forEach((section) => {
       observer.observe(section);
     });
