b91ab95ce55a07ca6fe8a68c5832f8970de544aa
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2025-02-07 21:02:00 +0900

    feat: 별이 움직이는 것 처럼 커서에 유성 애니메이션 적용

--- hooks/useFollowMove.ts
+++ hooks/useFollowMove.ts
@@ -3,17 +3,29 @@ import { useEffect } from "react";
 export function useFollowMouse(elementId: string) {
   useEffect(() => {
     const circle = document.getElementById(elementId);
+    if (!circle) return;
 
-    if (circle) {
-      circle.classList.remove("hidden");
-      const onMouseMove = (e: MouseEvent) => {
-        circle.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
-      };
-      document.addEventListener("mousemove", onMouseMove);
+    circle.classList.remove("hidden");
 
-      return () => {
-        document.removeEventListener("mousemove", onMouseMove);
-      };
-    }
-  }, []);
+    const onMouseMove = (e: MouseEvent) => {
+      circle.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
+
+      const trail = document.createElement("div");
+      trail.classList.add("circle-trail");
+
+      trail.style.left = `${e.clientX}px`;
+      trail.style.top = `${e.clientY}px`;
+
+      document.body.appendChild(trail);
+
+      setTimeout(() => {
+        trail.remove();
+      }, 500);
+    };
+
+    document.addEventListener("mousemove", onMouseMove);
+    return () => {
+      document.removeEventListener("mousemove", onMouseMove);
+    };
+  }, [elementId]);
 }
