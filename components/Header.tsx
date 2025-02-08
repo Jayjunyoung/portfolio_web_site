b12cc0e2dd858413af0d494f6c8934fa150151c7
Jayjunyoung <no2jfamily@tukorea.ac.kr>
2025-02-08 14:26:44 +0900

    design: 토글 메뉴 컴포넌트 요소 추가

--- components/Header.tsx
+++ components/Header.tsx
@@ -43,6 +43,7 @@ const Header: React.FC<HeaderProps> = ({
         <button onClick={toggleMenu} className="space-y-1">
           <span className="block w-6 h-0.5 bg-white"></span>
           <span className="block w-6 h-0.5 bg-white"></span>
+          <span className="block w-6 h-0.5 bg-white"></span>
         </button>
       </div>
 
